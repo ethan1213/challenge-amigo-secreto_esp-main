3ba8ffa1bdee8deea0d56ec68f487ace5272804f
Ethan <ethanastorga311@gmail.com>
2025-02-05 17:24:51 -0300

    Nuevos cambios y verificaciones listas

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+    const lista = document.getElementById("listaAmigos");
+
+    if (nombre === "") {
+        alert("Por favor, ingrese un nombre antes de añadir.");
+        return;
+    }
+
+    if (amigos.includes(nombre)) {
+        alert("Este nombre ya ha sido agregado.");
+        return;
+    }
+
+    amigos.push(nombre);
+
+    const li = document.createElement("li");
+    li.textContent = nombre;
+    lista.appendChild(li);
+
+    input.value = "";
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay nombres en la lista para sortear.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSecreto = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    const li = document.createElement("li");
+    li.textContent = `El amigo secreto es: ${amigoSecreto}`;
+    resultado.appendChild(li);
+}
